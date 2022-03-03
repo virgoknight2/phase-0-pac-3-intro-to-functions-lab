7b6b0cfd9f9d627bd9129ebe7502eb3dc409abf7
Lorelai Knutti <virgoknight2@gmail.com>
2022-03-02 21:15:40 -0500

    commit tree 99ac85048670ab2859c1c90056852e325f4419cc

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase(); 
+}
+"hello!".toUpperCase();
+function whisper(string) {
+    return string.toLowerCase();
+}
+"HELLO!".toLowerCase;
+
+function logShout(string){console.log(shout(string));}
+function logWhisper(string){console.log(whisper(string));}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()){
+        return "I can't hear you!"
+        
+    } if (string === string.toUpperCase()){return "YES INDEED!"}
+    if(string === "YES INDEED!"){return "Let's have dinner together!"}
+   else {return "I would love to!"}
+
+} 
